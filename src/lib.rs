011dd19519dbe6d38bf6012d5f2953ee97b8e846
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2026-02-09 02:38:52 +0000

    Added new FFI endpoints for media control

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,17 @@ mod logger;
 mod runner;
 
 use crate::ffi_types::*;
-use crate::runner::{LibrespotCommand, Runner};
+use crate::runner::{LibrespotCommand, Runner, RunnerState};
 use std::ffi::{CStr, c_char};
 use std::os::raw::c_void;
+use std::sync::Arc;
+use std::sync::atomic::Ordering;
 use std::thread;
-use tokio::runtime::Runtime;
 use tokio::sync::mpsc;
 
 pub struct LibrespotInstance {
     cmd_tx: mpsc::UnboundedSender<LibrespotCommand>,
+    state: Arc<RunnerState>,
     _thread_handle: thread::JoinHandle<()>,
 }
 
@@ -39,26 +41,24 @@ pub unsafe extern "C" fn librespot_new(
 
     let (tx, rx) = mpsc::unbounded_channel();
 
+    let runner_state = Arc::new(RunnerState::new());
+    let state_for_thread = runner_state.clone();
+
     let user_data_wrapper = UserDataWrapper(user_data);
 
-    let thread_handle = thread::spawn(move || {
-        let user_data_wrapper = user_data_wrapper;
-        let callback = callback;
-        let rx = rx;
-        let setup = setup;
-
-        let rt = Runtime::new().expect("Failed to create Tokio runtime");
+    let thread_handle = std::thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().expect("Failed to create Tokio runtime");
 
         rt.block_on(async move {
-            let raw_ptr = user_data_wrapper.0;
-
-            let mut runner = Runner::new(setup, rx, callback, raw_ptr);
+            let mut runner = Runner::new(setup, rx, callback, user_data_wrapper);
+            runner.state = state_for_thread;
             runner.run().await;
         });
     });
 
     let instance = Box::new(LibrespotInstance {
         cmd_tx: tx,
+        state: runner_state,
         _thread_handle: thread_handle,
     });
 
@@ -68,7 +68,15 @@ pub unsafe extern "C" fn librespot_new(
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn librespot_free(instance: *mut LibrespotInstance) {
     if !instance.is_null() {
-        let _ = unsafe { Box::from_raw(instance) };
+        unsafe {
+            let _ = Box::from_raw(instance);
+        }
+    }
+}
+
+unsafe fn send_cmd(instance: *mut LibrespotInstance, cmd: LibrespotCommand) {
+    if let Some(inst) = unsafe { instance.as_ref() } {
+        let _ = inst.cmd_tx.send(cmd);
     }
 }
 
@@ -103,44 +111,79 @@ pub unsafe extern "C" fn librespot_prev(instance: *mut LibrespotInstance) {
 }
 
 #[unsafe(no_mangle)]
+pub unsafe extern "C" fn librespot_seek(instance: *mut LibrespotInstance, pos_ms: u32) {
+    unsafe { send_cmd(instance, LibrespotCommand::Seek(pos_ms)) };
+}
+
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn librespot_set_volume(instance: *mut LibrespotInstance, volume: u16) {
     unsafe { send_cmd(instance, LibrespotCommand::SetVolume(volume)) };
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn librespot_seek(instance: *mut LibrespotInstance, position_ms: u32) {
-    unsafe { send_cmd(instance, LibrespotCommand::Seek(position_ms)) };
+pub unsafe extern "C" fn librespot_set_shuffle(instance: *mut LibrespotInstance, enabled: bool) {
+    unsafe { send_cmd(instance, LibrespotCommand::SetShuffle(enabled)) };
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn librespot_start_discovery(instance: *mut LibrespotInstance) {
-    unsafe { send_cmd(instance, LibrespotCommand::StartDiscovery) };
-}
-
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn librespot_update_credentials(
-    instance: *mut LibrespotInstance,
-    username: *const c_char,
-    auth_data: *const c_char,
-) {
-    let u = unsafe { CStr::from_ptr(username).to_string_lossy().into_owned() };
-    let a = unsafe { CStr::from_ptr(auth_data).to_string_lossy().into_owned() };
-
+pub unsafe extern "C" fn librespot_set_repeat(instance: *mut LibrespotInstance, mode: u32) {
     unsafe {
-        send_cmd(
-            instance,
-            LibrespotCommand::UpdateCredentials {
-                username: u,
-                auth_data: a,
-            },
-        );
+        send_cmd(instance, LibrespotCommand::SetRepeatContext(mode >= 1));
+        send_cmd(instance, LibrespotCommand::SetRepeatTrack(mode == 2));
     }
 }
 
-unsafe fn send_cmd(instance: *mut LibrespotInstance, cmd: LibrespotCommand) {
-    unsafe {
-        if let Some(inst) = instance.as_ref() {
-            let _ = inst.cmd_tx.send(cmd);
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn librespot_get_position_ms(instance: *mut LibrespotInstance) -> u32 {
+    if let Some(inst) = unsafe { instance.as_ref() } {
+        let state = &inst.state;
+
+        let base_ms = state.position_ms.load(Ordering::Acquire);
+        let sync_wp = state.sync_write_pos.load(Ordering::Acquire);
+
+        let current_wp = librespot_playback::audio_backend::get_write_pos();
+
+        if current_wp < sync_wp {
+            return base_ms;
+        }
+        let bytes_delta = current_wp - sync_wp;
+
+        let sample_rate = 44100.0;
+        let channels = 2.0;
+        let bytes_per_ms = (sample_rate * channels * 2.0) / 1000.0;
+
+        if bytes_per_ms <= 0.0 {
+            return base_ms;
+        }
+
+        let drift_ms = (bytes_delta as f32 / bytes_per_ms) as u32;
+        return base_ms + drift_ms;
+    }
+    0
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn librespot_get_duration_ms(instance: *mut LibrespotInstance) -> u32 {
+    unsafe { instance.as_ref() }.map_or(0, |i| i.state.duration_ms.load(Ordering::Acquire))
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn librespot_get_current_track_info(
+    instance: *mut LibrespotInstance,
+) -> TrackMetadata {
+    if let Some(inst) = unsafe { instance.as_ref() } {
+        if let Ok(current) = inst.state.current_track.read() {
+            if let Some(ref meta) = *current {
+                return TrackMetadata {
+                    uri: meta.uri.as_ptr(),
+                    name: meta.name.as_ptr(),
+                    artist: meta.artist.as_ptr(),
+                    album: meta.album.as_ptr(),
+                    cover_url: meta.cover_url.as_ptr(),
+                    duration_ms: meta.duration_ms,
+                };
+            }
         }
     }
+    unsafe { std::mem::zeroed() }
 }
--- src/runner.rs
+++ src/runner.rs
@@ -1,4 +1,4 @@
-use crate::ffi_types::*;
+use crate::{UserDataWrapper, ffi_types::*};
 use futures_util::StreamExt;
 use librespot_connect::{ConnectConfig, Spirc};
 use librespot_core::{
@@ -11,8 +11,11 @@ use librespot_playback::{
     mixer::{self, MixerConfig},
     player::{Player, PlayerEvent},
 };
-use std::ffi::{CString, c_void};
 use std::pin::Pin;
+use std::sync::Arc;
+use std::sync::RwLock;
+use std::sync::atomic::{AtomicBool, AtomicU16, AtomicU32, Ordering};
+use std::{ffi::CString, sync::atomic::AtomicUsize};
 use tokio::sync::mpsc;
 
 pub enum LibrespotCommand {
@@ -23,6 +26,9 @@ pub enum LibrespotCommand {
     Prev,
     SetVolume(u16),
     Seek(u32),
+    SetShuffle(bool),
+    SetRepeatContext(bool),
+    SetRepeatTrack(bool),
     StartDiscovery,
     UpdateCredentials { username: String, auth_data: String },
     Stop,
@@ -42,11 +48,47 @@ pub struct RunnerSetup {
     pub zeroconf_port: u16,
 }
 
+pub struct TrackMetadataInternal {
+    pub uri: CString,
+    pub name: CString,
+    pub artist: CString,
+    pub album: CString,
+    pub cover_url: CString,
+    pub duration_ms: u32,
+}
+
+pub struct RunnerState {
+    pub is_playing: AtomicBool,
+    pub shuffle: AtomicBool,
+    pub repeat: AtomicU32, // 0: Off, 1: Context, 2: Track
+    pub position_ms: AtomicU32,
+    pub sync_write_pos: AtomicUsize,
+    pub duration_ms: AtomicU32,
+    pub volume: AtomicU16,
+    pub current_track: RwLock<Option<TrackMetadataInternal>>,
+}
+
+impl RunnerState {
+    pub fn new() -> Self {
+        Self {
+            is_playing: AtomicBool::new(false),
+            shuffle: AtomicBool::new(false),
+            repeat: AtomicU32::new(0),
+            position_ms: AtomicU32::new(0),
+            sync_write_pos: AtomicUsize::new(0),
+            duration_ms: AtomicU32::new(0),
+            volume: AtomicU16::new(0),
+            current_track: RwLock::new(None),
+        }
+    }
+}
+
 pub struct Runner {
     setup: RunnerSetup,
     cmd_rx: mpsc::UnboundedReceiver<LibrespotCommand>,
     callback: LibrespotCallback,
-    user_data: *mut c_void,
+    user_data: UserDataWrapper,
+    pub state: Arc<RunnerState>,
 }
 
 impl Runner {
@@ -54,32 +96,19 @@ impl Runner {
         setup: RunnerSetup,
         cmd_rx: mpsc::UnboundedReceiver<LibrespotCommand>,
         callback: LibrespotCallback,
-        user_data: *mut c_void,
+        user_data: UserDataWrapper,
     ) -> Self {
-        log::info!("Runner::new()");
-        log::info!("  device_name={}", setup.device_name);
-        log::info!("  device_type={:?}", setup.device_type);
-        log::info!("  enable_discovery={}", setup.enable_discovery);
-        log::info!("  audio_format={:?}", setup.audio_format);
-        log::info!("  player.bitrate={:?}", setup.player_config.bitrate);
-        log::info!(
-            "  player.normalisation={}",
-            setup.player_config.normalisation
-        );
-        log::info!("  mixer.volume_ctrl={:?}", setup.mixer_config.volume_ctrl);
-        log::info!("  tmp_dir={:?}", setup.session_config.tmp_dir);
-        log::info!("  initial_creds={}", setup.initial_creds.is_some());
-
         Self {
             setup,
             cmd_rx,
             callback,
             user_data,
+            state: Arc::new(RunnerState::new()),
         }
     }
 
     fn emit(&self, event: LibrespotEvent) {
-        unsafe { (self.callback)(&event, self.user_data) };
+        (self.callback)(&event, self.user_data.0);
     }
 
     pub async fn run(&mut self) {
@@ -93,20 +122,11 @@ impl Runner {
         )
         .ok();
 
-        log::info!("Cache initialised");
-
         let mut session = Session::new(self.setup.session_config.clone(), cache.clone());
-        log::info!("Session created");
-
         let mixer_builder = mixer::find(None).expect("No mixer found");
         let mixer = mixer_builder(self.setup.mixer_config.clone()).expect("Failed to create mixer");
-        log::info!("Mixer created");
-
         let backend = self.setup.audio_backend;
-        log::info!("Audio backend resolved");
-
         let format = self.setup.audio_format;
-        log::info!("Audio format for backend: {:?}", format);
 
         let player = Player::new(
             self.setup.player_config.clone(),
@@ -115,15 +135,12 @@ impl Runner {
             move || (backend)(None, format),
         );
 
-        log::info!("Player created");
-
         let mut spirc: Option<Spirc> = None;
         let mut spirc_task: Option<Pin<Box<dyn std::future::Future<Output = ()> + Send>>> = None;
         let mut discovery: Option<Discovery> = None;
         let mut player_rx = player.get_player_event_channel();
 
         if self.setup.enable_discovery {
-            log::info!("Starting discovery service");
             discovery = Discovery::builder(
                 self.setup.session_config.device_id.clone(),
                 self.setup.session_config.client_id.clone(),
@@ -138,75 +155,50 @@ impl Runner {
         let mut last_creds = self.setup.initial_creds.clone();
         let mut connecting = last_creds.is_some();
 
-        log::info!("Runner main loop starting");
-
         loop {
             tokio::select! {
+                // UI/Hardware Commands
                 Some(cmd) = self.cmd_rx.recv() => {
                     match cmd {
-                        LibrespotCommand::Stop => {
-                            log::info!("Command: Stop");
-                            player.stop();
-                        }
-                        LibrespotCommand::Play => {
-                            log::info!("Command: Play");
-                            player.play();
-                        }
-                        LibrespotCommand::Pause => {
-                            log::info!("Command: Pause");
-                            player.pause();
-                        }
-                        LibrespotCommand::Next => {
-                            log::info!("Command: Next");
-                            if let Some(ref s) = spirc { s.next(); }
-                        }
-                        LibrespotCommand::Prev => {
-                            log::info!("Command: Prev");
-                            if let Some(ref s) = spirc { s.prev(); }
-                        }
+                        LibrespotCommand::Stop => player.stop(),
+                        LibrespotCommand::Play => player.play(),
+                        LibrespotCommand::Pause => player.pause(),
+                        LibrespotCommand::Next => { if let Some(ref s) = spirc { s.next(); } }
+                        LibrespotCommand::Prev => { if let Some(ref s) = spirc { s.prev(); } }
+                        LibrespotCommand::Seek(ms) => player.seek(ms),
                         LibrespotCommand::SetVolume(v) => {
-                            log::info!("Command: SetVolume({})", v);
                             mixer.set_volume(v);
+                            self.state.volume.store(v, Ordering::Relaxed);
                             player.emit_volume_changed_event(v);
                         }
-                        LibrespotCommand::Seek(ms) => {
-                            log::info!("Command: Seek({})", ms);
-                            player.seek(ms);
+                        LibrespotCommand::SetShuffle(enabled) => {
+                            if let Some(ref s) = spirc { let _ = s.shuffle(enabled); }
+                        }
+                        LibrespotCommand::SetRepeatContext(enabled) => {
+                            if let Some(ref s) = spirc { let _ = s.repeat(enabled); }
+                        }
+                        LibrespotCommand::SetRepeatTrack(enabled) => {
+                            if let Some(ref s) = spirc { let _ = s.repeat_track(enabled); }
                         }
                         LibrespotCommand::Load { uri, play } => {
-                            log::info!("Command: Load(uri={}, play={})", uri, play);
                             if let Ok(track_uri) = SpotifyUri::from_uri(&uri) {
                                 player.load(track_uri, play, 0);
-                            } else {
-                                log::warn!("Invalid Spotify URI: {}", uri);
-                            }
-                        }
-                        LibrespotCommand::StartDiscovery => {
-                            log::info!("Command: StartDiscovery");
-                            if discovery.is_none() {
-                                discovery = Discovery::builder(
-                                    self.setup.session_config.device_id.clone(),
-                                    self.setup.session_config.client_id.clone(),
-                                )
-                                .name(self.setup.device_name.clone())
-                                .device_type(self.setup.device_type)
-                                .launch()
-                                .ok();
                             }
                         }
                         LibrespotCommand::UpdateCredentials { username, auth_data } => {
-                            log::info!("Command: UpdateCredentials(user={})", username);
                             last_creds = Some(Credentials::with_password(username, auth_data));
                             connecting = true;
                         }
+                        LibrespotCommand::StartDiscovery => {}
                     }
                 }
 
+                // Events coming from Librespot
                 Some(event) = player_rx.recv() => {
-                    log::info!("PlayerEvent received: {:?}", event);
                     self.handle_player_event(event);
                 }
 
+                // Discovery and Session Management
                 creds_opt = async {
                     match discovery.as_mut() {
                         Some(d) => d.next().await,
@@ -214,27 +206,19 @@ impl Runner {
                     }
                 } => {
                     if let Some(creds) = creds_opt {
-                        log::info!("Discovery provided credentials");
                         last_creds = Some(creds);
                         connecting = true;
                     }
                 }
 
                 _ = async {}, if connecting && last_creds.is_some() => {
-                    log::info!("Connecting with new credentials");
-
                     if session.is_invalid() {
-                        log::info!("Session invalid, recreating");
                         session = Session::new(self.setup.session_config.clone(), cache.clone());
                         player.set_session(session.clone());
                     }
 
                     if let Some(c) = last_creds.clone() {
-                        if let Some(s) = spirc.take() {
-                            log::info!("Shutting down old Spirc");
-                            let _ = s.shutdown();
-                        }
-
+                        if let Some(s) = spirc.take() { let _ = s.shutdown(); }
                         let spirc_res = Spirc::new(
                             self.setup.connect_config.clone(),
                             session.clone(),
@@ -245,18 +229,12 @@ impl Runner {
 
                         match spirc_res {
                             Ok((s, task)) => {
-                                log::info!("Spirc connected");
                                 spirc = Some(s);
                                 spirc_task = Some(Box::pin(task));
-                                self.emit(LibrespotEvent {
-                                    event_type: EventType::SessionConnected,
-                                    data: unsafe { std::mem::zeroed() }
-                                });
+                                self.emit(LibrespotEvent { event_type: EventType::SessionConnected, data: unsafe { std::mem::zeroed() } });
                                 connecting = false;
                             }
-                            Err(e) => {
-                                log::error!("Spirc connection failed: {:?}", e);
-                            }
+                            Err(e) => log::error!("Spirc connection failed: {:?}", e),
                         }
                     }
                 }
@@ -264,13 +242,9 @@ impl Runner {
                 _ = async {
                     if let Some(t) = spirc_task.as_mut() { t.await; }
                 }, if spirc_task.is_some() => {
-                    log::warn!("Spirc task ended");
                     spirc_task = None;
                     spirc = None;
-                    self.emit(LibrespotEvent {
-                        event_type: EventType::SessionDisconnected,
-                        data: unsafe { std::mem::zeroed() }
-                    });
+                    self.emit(LibrespotEvent { event_type: EventType::SessionDisconnected, data: unsafe { std::mem::zeroed() } });
                 }
             }
         }
@@ -278,41 +252,70 @@ impl Runner {
 
     fn handle_player_event(&self, event: PlayerEvent) {
         match event {
-            PlayerEvent::TrackChanged { audio_item } => {
-                let c_uri = CString::new(audio_item.uri).unwrap_or_default();
-                let c_name = CString::new(audio_item.name).unwrap_or_default();
-                let empty = CString::new("").unwrap();
+            PlayerEvent::Playing { position_ms, .. }
+            | PlayerEvent::Paused { position_ms, .. }
+            | PlayerEvent::PositionCorrection { position_ms, .. }
+            | PlayerEvent::Seeked { position_ms, .. }
+            | PlayerEvent::PositionChanged { position_ms, .. } => {
+                let current_wp = librespot_playback::audio_backend::get_write_pos();
+                self.state.position_ms.store(position_ms, Ordering::Release);
+                self.state
+                    .sync_write_pos
+                    .store(current_wp, Ordering::Release);
 
-                let meta = TrackMetadata {
-                    uri: c_uri.as_ptr(),
-                    name: c_name.as_ptr(),
-                    artist: empty.as_ptr(),
-                    album: empty.as_ptr(),
-                    cover_url: empty.as_ptr(),
-                    duration_ms: audio_item.duration_ms as u32,
+                if let PlayerEvent::Playing { .. } = event {
+                    self.state.is_playing.store(true, Ordering::Release);
+                    self.emit(LibrespotEvent {
+                        event_type: EventType::PlaybackResumed,
+                        data: unsafe { std::mem::zeroed() },
+                    });
+                } else if let PlayerEvent::Paused { .. } = event {
+                    self.state.is_playing.store(false, Ordering::Release);
+                    self.emit(LibrespotEvent {
+                        event_type: EventType::PlaybackPaused,
+                        data: unsafe { std::mem::zeroed() },
+                    });
+                }
+            }
+            PlayerEvent::TrackChanged { audio_item } => {
+                let duration = audio_item.duration_ms as u32;
+                self.state.duration_ms.store(duration, Ordering::Relaxed);
+
+                let internal = TrackMetadataInternal {
+                    uri: CString::new(audio_item.uri.clone()).unwrap_or_default(),
+                    name: CString::new(audio_item.name.clone()).unwrap_or_default(),
+                    artist: CString::new("").unwrap(),
+                    album: CString::new("").unwrap(),
+                    cover_url: CString::new("").unwrap(),
+                    duration_ms: duration,
                 };
 
-                let evt = LibrespotEvent {
+                let meta = TrackMetadata {
+                    uri: internal.uri.as_ptr(),
+                    name: internal.name.as_ptr(),
+                    artist: internal.artist.as_ptr(),
+                    album: internal.album.as_ptr(),
+                    cover_url: internal.cover_url.as_ptr(),
+                    duration_ms: duration,
+                };
+
+                if let Ok(mut current) = self.state.current_track.write() {
+                    *current = Some(internal);
+                }
+
+                self.emit(LibrespotEvent {
                     event_type: EventType::TrackChanged,
                     data: EventData {
                         track: std::mem::ManuallyDrop::new(meta),
                     },
-                };
-                self.emit(evt);
-            }
-            PlayerEvent::Paused { .. } => {
-                self.emit(LibrespotEvent {
-                    event_type: EventType::PlaybackPaused,
-                    data: unsafe { std::mem::zeroed() },
                 });
             }
-            PlayerEvent::Playing { .. } => {
-                self.emit(LibrespotEvent {
-                    event_type: EventType::PlaybackResumed,
-                    data: unsafe { std::mem::zeroed() },
-                });
+            PlayerEvent::Stopped { .. } => {
+                self.state.is_playing.store(false, Ordering::Release);
+                self.state.position_ms.store(0, Ordering::Release);
             }
             PlayerEvent::VolumeChanged { volume } => {
+                self.state.volume.store(volume, Ordering::Relaxed);
                 self.emit(LibrespotEvent {
                     event_type: EventType::VolumeChanged,
                     data: EventData { volume },
